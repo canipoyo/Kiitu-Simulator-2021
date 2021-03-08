a7cc4603cfb4d3d4449a56605fa676a3ed18b565
iriSVakjyrie <seiryuu2772@gmail.com>
2021-03-09 02:05:39 +0900

    ツイート画面を追加

---
+++ Assets/Scripts/Tweet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tweet : MonoBehaviour
+{
+
+    [SerializeField] GameObject tweet;
+
+    [SerializeField] GameObject Parent;
+
+    [SerializeField] Text tweetNum;
+
+    [SerializeField] ScrollRect tweetber;
+
+    List<GameObject> Tweets = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int tweetsnum = Tweets.Count;
+
+        tweetNum.text = "ツイート数:" + tweetsnum + "ツイート";
+    }
+
+
+    public void OnClickTweetButton()
+    {
+
+        Tweets.Add(Instantiate( tweet,new Vector3(0,0,0),Quaternion.identity,Parent.transform ));
+
+        tweetber.verticalNormalizedPosition = 1.01f;
+
+    }
+}
