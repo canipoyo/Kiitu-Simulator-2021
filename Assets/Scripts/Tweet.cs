517123ff131504070af94093ba32e46d8761193f
iriSVakjyrie <seiryuu2772@gmail.com>
2021-04-02 22:54:26 +0900

    tweetの構造を規定

--- Assets/Scripts/Tweet.cs
+++ Assets/Scripts/Tweet.cs
@@ -1,60 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Tweet : MonoBehaviour
+
+[System.Serializable]
+
+public class tweet
 {
+    
+    
+    
+    public int favorite_count;
 
-    [SerializeField] GameObject[] tweet;
+    public int retweet_count;
 
-    [SerializeField] GameObject Parent;
+    public string created_at;
+
+    public string full_text;
 
    
 
-    [SerializeField] ScrollRect tweetber;
 
-    [SerializeField]List<GameObject> Tweets = new List<GameObject>();
-
-    [SerializeField]GameManager gameManager;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-
-        
-    }
-
-
-    public void OnClickTweetButton()
-    {
-
-        int randnum = Random.Range(0, tweet.Length);
-        Debug.Log(randnum + "番目を生成");
-        if(Tweets.Count >= 5)
-        {
-
-            Destroy(Tweets[0]);
-
-            Tweets.RemoveAt(0);
-
-
-        }
-
-
-        Tweets.Add(Instantiate( tweet[randnum],new Vector3(0,0,0),Quaternion.identity,Parent.transform ));
-
-        gameManager.TweetNum += 1;
-
-        tweetber.verticalNormalizedPosition = 1.1f;
-
-        
-    }
 }
---
+++ Assets/Scripts/Tweets.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tweets : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] tweet;
+
+    [SerializeField] GameObject Parent;
+
+   
+
+    [SerializeField] ScrollRect tweetber;
+
+    [SerializeField]List<GameObject> Tweet = new List<GameObject>();
+
+    [SerializeField]GameManager gameManager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        
+    }
+
+
+    public void OnClickTweetButton()
+    {
+
+        int randnum = Random.Range(0, tweet.Length);
+        Debug.Log(randnum + "番目を生成");
+        if(Tweet.Count >= 5)
+        {
+
+            Destroy(Tweet[0]);
+
+            Tweet.RemoveAt(0);
+
+
+        }
+
+
+        Tweet.Add(Instantiate( tweet[randnum],new Vector3(0,0,0),Quaternion.identity,Parent.transform ));
+
+        gameManager.TweetNum += 1;
+
+        tweetber.verticalNormalizedPosition = 1.1f;
+
+        
+    }
+}
